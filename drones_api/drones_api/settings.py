f07cb1209e322cf31f3032bdcf9e36451c01fe8f
Ernesto Figueredo <ernesto@jewelersconnect.com>
2022-09-27 03:03:27 -0400

    Adding CORS config to Settings

--- drones_api/drones_api/settings.py
+++ drones_api/drones_api/settings.py
@@ -39,16 +39,18 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'drones',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',        
 ]
 
 REST_FRAMEWORK = {
@@ -58,6 +60,10 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
 }
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = (    
+)
 
 ROOT_URLCONF = 'drones_api.urls'
 
