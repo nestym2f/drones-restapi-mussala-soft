1a02eb278f649a87073f86884a8a1e0d56fab39b
Ernesto Figueredo <ernesto@jewelersconnect.com>
2022-09-27 03:25:27 -0400

    Add Unique attributes to models

--- drones_api/drones/models.py
+++ drones_api/drones/models.py
@@ -8,7 +8,7 @@ class State(models.Model):
 class Image(models.Model):
     url = models.CharField(max_length=255)
 class Drone(models.Model):
-    serialNumber = models.CharField(max_length=100, db_column='serial_number')
+    serialNumber = models.CharField(max_length=100, db_column='serial_number', unique=True)
     model = models.ForeignKey(Model, null=True, on_delete=models.SET_NULL)
     weightLimit = models.DecimalField(default=0.1, max_digits=5, decimal_places=2, db_column='weight_limit')
     batteryCapacity = models.PositiveSmallIntegerField(default=0, db_column='battery_capacity')
@@ -16,6 +16,6 @@ class Drone(models.Model):
 class Medication(models.Model):
     name = models.CharField(max_length=255)
     weight = models.DecimalField(default=0.1, max_digits=5, decimal_places=2)
-    code = models.CharField(max_length=255);
+    code = models.CharField(max_length=255, unique=True);
     image = models.ForeignKey(Image, on_delete=models.CASCADE)
     drone = models.ForeignKey(Drone, null=True, on_delete=models.SET_NULL)
