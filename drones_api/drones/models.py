23ae9b031b13e13d5f3b9fd6a49d792c747e4c2f
Ernesto Figueredo <ernesto@jewelersconnect.com>
2022-09-27 00:05:28 -0400

    Added manytoone relation to Medication Model

--- drones_api/drones/models.py
+++ drones_api/drones/models.py
@@ -13,12 +13,13 @@ class Image(models.Model):
     url = models.CharField(max_length=255)
 class Drone(models.Model):
     serialNumber = models.CharField(max_length=100, db_column='serial_number')
-    model = models.ForeignKey(Model,null=True, on_delete=models.SET_NULL)
+    model = models.ForeignKey(Model, null=True, on_delete=models.SET_NULL)
     weightLimit = models.DecimalField(default=0.1, max_digits=5, decimal_places=2, db_column='weight_limit')
     batteryCapacity = models.PositiveSmallIntegerField(default=0, db_column='battery_capacity')
-    state = models.ForeignKey(State,null=True, on_delete=models.SET_NULL)    
+    state = models.ForeignKey(State, null=True, on_delete=models.SET_NULL)    
 class Medication(models.Model):
     name = models.CharField(max_length=255)
     weight = models.DecimalField(default=0.1, max_digits=5, decimal_places=2)
     code = models.CharField(max_length=255);
     image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    drone = models.ForeignKey(Drone, null=True, on_delete=models.SET_NULL)
