12095eab1a75bd7f9f34946f6c1527069e9cc5c8
Ernesto Figueredo <ernesto@jewelersconnect.com>
2022-09-28 13:53:43 -0400

    Added Get All/Delete All Drones Views (GET, DELETE)

--- drones_api/drones/views.py
+++ drones_api/drones/views.py
@@ -1,3 +1,23 @@
+from ast import parse
 from django.shortcuts import render
+from django.http.response import JsonResponse
+from .models import Model, State, Image, Drone, Medication
+from .serializers import DroneSerializer, MedicationSerializer
+from rest_framework import viewsets, permissions, status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.parsers import JSONParser
+    
+@api_view(['GET'])
+@permission_classes((permissions.AllowAny,))
+def dronesGetAllView(request):    
+    queryset = Drone.objects.all()        
+    droneSerializer = DroneSerializer(queryset, many=True)
+    return JsonResponse(droneSerializer.data, safe=False)    
 
-# Create your views here.
+
+
+@api_view(['DELETE'])
+@permission_classes((permissions.AllowAny,))
+def dronesDeleteAllView(request):    
+    count = Drone.objects.all().delete()        
+    return JsonResponse({'message': '{} Drones were deleted successfully!'.format(count[0])}, status=status.HTTP_200_OK)
