e4da4d8bb61a794709b2da4e55fe71c3d89369b9
Ernesto Figueredo <ernesto@jewelersconnect.com>
2022-09-28 14:08:24 -0400

    Remove Nullable in Models 'model' and 'state'/ migrations executed

--- drones_api/drones/models.py
+++ drones_api/drones/models.py
@@ -9,10 +9,10 @@ class Image(models.Model):
     url = models.CharField(max_length=255)
 class Drone(models.Model):
     serialNumber = models.CharField(max_length=100, db_column='serial_number', unique=True)
-    model = models.ForeignKey(Model, null=True, on_delete=models.SET_NULL)
+    model = models.ForeignKey(Model, on_delete=models.DO_NOTHING)
     weightLimit = models.DecimalField(default=0.1, max_digits=5, decimal_places=2, db_column='weight_limit')
     batteryCapacity = models.PositiveSmallIntegerField(default=0, db_column='battery_capacity')
-    state = models.ForeignKey(State, null=True, on_delete=models.SET_NULL)    
+    state = models.ForeignKey(State, on_delete=models.DO_NOTHING)    
 class Medication(models.Model):
     name = models.CharField(max_length=255)
     weight = models.DecimalField(default=0.1, max_digits=5, decimal_places=2)
