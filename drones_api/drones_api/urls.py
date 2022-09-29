20bbb88f03a4be26e8c67a4c0cb45b264ebd9661
Ernesto Figueredo <ernesto@jewelersconnect.com>
2022-09-29 02:13:08 -0400

    Added/Modified URLs

--- drones_api/drones_api/urls.py
+++ drones_api/drones_api/urls.py
@@ -10,10 +10,12 @@ urlpatterns = [
     url(r'^api/drones/$', views.dronesGetAllView),    
     url(r'^api/drones/delete', views.dronesDeleteAllView),
     url(r'^api/drones/register', views.dronesRegisterView),  
-    url(r'^api/drones/(?P<pk>[0-9]+)/$', views.droneDetailView),
+    url(r'^api/drones/id/(?P<pk>[0-9]+)/$', views.droneDetailView),
     url(r'^api/drones/serial-number/(?P<serialNumber>\w+)/$', views.droneDetailView),
     url(r'^api/medications/$', views.medicationGetAllView),
-    url(r'^api/medications/delete', views.dronesDeleteAllView),
+    url(r'^api/medications/delete', views.medicationDeleteAllView),
+    url(r'^api/medications/register', views.medicationRegisterView),
+    
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
