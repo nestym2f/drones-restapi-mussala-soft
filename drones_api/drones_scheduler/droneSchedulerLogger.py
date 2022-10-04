9e7e1c59b4cdd8bfc8f930a690dc1a6a3e3a3cf1
Ernesto Figueredo <ernesto@jewelersconnect.com>
2022-10-04 00:29:10 -0400

    Added function to call the scheduler updated gitignore updated db fixed callable function

--- drones_api/drones_scheduler/droneSchedulerLogger.py
+++ drones_api/drones_scheduler/droneSchedulerLogger.py
@@ -3,7 +3,7 @@ from drones.views import createAuditLogWithScheduler
 
 def startAuditLogWithScheduler():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(createAuditLogWithScheduler(),'interval',minutes=1,id='batteryLogger',replace_existing=True)
+    scheduler.add_job(createAuditLogWithScheduler ,'interval',minutes=1,id='batteryLogger',replace_existing=True)
     scheduler.start()
 
 
